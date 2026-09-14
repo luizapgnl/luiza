cb953835ecd6c7c8f54471bb21624ab1bd6526c3
luiza :) <138608726+luizapgnl@users.noreply.github.com>
2026-09-14 16:52:21 -0300

    thr
    hrth

--- index.php
+++ index.php
@@ -4,27 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hi haters</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
 
-<?php
-echo "<h1> Hi haters </h1>";
+<div class="card">
+    <?php
+    echo "<h1> Hi haters </h1>";
 
-$nome = "luiza";
-$idade = 17; 
-?>
+    $nome = "luiza";
+    $idade = 17; 
+    ?>
 
-<h1>Nome: <?= $nome ?></h1>
-<p>Idade: <?= $idade ?></p>
+    <h1>Nome: <?= ucfirst($nome) ?></h1>
+    <p>Idade: <?= $idade ?></p>
 
-<p>
-    Status: 
-    <?php if ($idade >= 18): ?>
-        <strong style="color: pink;">Maior de idade</strong>
-    <?php else: ?>
-        <strong style="color: red;">Menor de idade</strong>
-    <?php endif; ?>
-</p>
+    <p>
+        Status: 
+        <?php if ($idade >= 18): ?>
+            <strong style="color: pink;">Maior de idade</strong>
+        <?php else: ?>
+            <strong style="color: red;">Menor de idade</strong>
+        <?php endif; ?>
+    </p>
+</div>
 
 </body>
 </html>
