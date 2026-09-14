98a980d7370f11a2c9d3c4d8bb9e8cb2ff69d167
luiza :) <138608726+luizapgnl@users.noreply.github.com>
2026-09-14 16:35:07 -0300

    luiza
    alteração idade

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
+<!DOCTYPE html>
+<html lang="PT-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hi haters</title>
+</head>
+<body>
+
 <?php
 echo "<h1> Hi haters </h1>";
+
+$nome = "luiza";
+$idade = 17; 
+?>
+
+<h1>Nome: <?= $nome ?></h1>
+<p>Idade: <?= $idade ?></p>
+
+<p>
+    Status: 
+    <?php if ($idade >= 18): ?>
+        <strong style="color: green;">Maior de idade</strong>
+    <?php else: ?>
+        <strong style="color: red;">Menor de idade</strong>
+    <?php endif; ?>
+</p>
+
+</body>
+</html>
